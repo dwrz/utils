cc9847628c80be1f6877c86d300248d5d7b9ff6b
dwrz <dwrz@dwrz.net>
2019-12-21 19:31:02 +0000

    Add cmd/age

---
+++ cmd/age/age.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"time"
+)
+
+const (
+	SECONDS_IN_DAY  = 86400
+	SECONDS_IN_WEEK = 604800
+)
+
+var (
+	now = time.Now()
+
+	year  = flag.Int("year", 0, "birth year")
+	month = flag.Int("month", 0, "birth month")
+	day   = flag.Int("day", 0, "birth day")
+
+	date = flag.String(
+		"date",
+		"",
+		"the RFC3339 date and time from which to measure age",
+	)
+	output = flag.String(
+		"output",
+		"years",
+		"the duration to output [years|months|weeks|days|seconds]",
+	)
+)
+
+func main() {
+	flag.Parse()
+
+	if *year <= 0 {
+		fmt.Fprintf(os.Stderr, "invalid year: %d\n", *year)
+		return
+	}
+	if *month <= 0 || *month > 12 {
+		fmt.Fprintf(os.Stderr, "invalid month: %d\n", *month)
+		return
+	}
+	if *day <= 0 || *day > 31 {
+		fmt.Fprintf(os.Stderr, "invalid day: %d\n", *day)
+		return
+	}
+
+	if *date != "" {
+		var err error
+		now, err = time.Parse(time.RFC3339, *date)
+		if err != nil {
+			fmt.Fprintf(
+				os.Stderr, "failed to parse date: %v\n", err,
+			)
+			return
+		}
+	}
+
+	birthMonth := time.Date(
+		*year, time.Month(*month), 1, 0, 0, 0, 0, time.Local,
+	)
+	lastDayOfBirthMonth := birthMonth.
+		AddDate(0, 1, 0).Add(-time.Nanosecond).Day()
+	if *day > lastDayOfBirthMonth {
+		fmt.Fprintf(
+			os.Stderr,
+			"invalid day: %d is after last day of month %d\n",
+			*day, lastDayOfBirthMonth,
+		)
+		return
+	}
+
+	birthday := time.Date(
+		*year, time.Month(*month), *day, 0, 0, 0, 0, time.Local,
+	)
+
+	if now.Before(birthday) {
+		fmt.Fprintf(os.Stderr, "cannot measure negative age")
+		return
+	}
+
+	ageInSeconds := int(now.Sub(birthday).Seconds())
+	ageInCalendarYears := now.Year() - birthday.Year()
+	birthdayPassed := func() bool {
+		switch {
+		case now.Month() > birthday.Month(),
+			now.Month() == birthday.Month() &&
+				now.Day() >= birthday.Day():
+			return true
+		default:
+			return false
+		}
+	}()
+
+	switch *output {
+
+	case "years":
+		if birthdayPassed {
+			fmt.Println(ageInCalendarYears)
+			return
+		}
+		fmt.Println(ageInCalendarYears - 1)
+
+	case "months":
+		yearsInMonths := (ageInCalendarYears * 12)
+		ageInCalendarMonths := int(now.Month() - birthday.Month())
+		switch {
+		case birthdayPassed && now.Day() > birthday.Day():
+			fmt.Println(yearsInMonths + ageInCalendarMonths)
+			return
+		case birthdayPassed && now.Day() < birthday.Day():
+			fmt.Println(yearsInMonths + ageInCalendarMonths - 1)
+			return
+		case !birthdayPassed && now.Day() > birthday.Day():
+			fmt.Println(yearsInMonths - ageInCalendarMonths)
+			return
+		case !birthdayPassed && now.Day() < birthday.Day():
+			fmt.Println(yearsInMonths - ageInCalendarMonths - 1)
+			return
+		}
+
+	case "weeks":
+		fmt.Println(ageInSeconds / SECONDS_IN_WEEK)
+
+	case "days":
+		fmt.Println(ageInSeconds / SECONDS_IN_DAY)
+
+	case "seconds":
+		fmt.Println(ageInSeconds)
+
+	default:
+		fmt.Fprintf(
+			os.Stderr, "unrecognized output flag: %s\n", *output,
+		)
+		return
+	}
+}
