91cfa2a542ca585ae195ec5218726fc12fc0ed31
dwrz <dwrz@dwrz.net>
2019-12-21 19:31:16 +0000

    Add cmd/age test placeholders

---
+++ cmd/age/age_test.go
@@ -0,0 +1,28 @@
+package main
+
+import "testing"
+
+type ageTest struct {
+	Date     string
+	Day      string
+	Expected int
+	Month    string
+	Output   string
+	Year     string
+}
+
+func TestAgeYear(t *testing.T) {
+	var ageTests = []ageTest{}
+
+	for _, test := range ageTests {
+		// TODO
+	}
+}
+
+func TestAgeMonth(t *testing.T) {
+	var ageTests = []ageTest{}
+
+	for _, test := range ageTests {
+		// TODO
+	}
+}
